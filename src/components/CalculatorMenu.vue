3e002d505a54e659dd1cdd7125a1aa305e800c9c
Vitor Pires <88112014+vikpires@users.noreply.github.com>
2024-06-11 16:50:11 -0300

    add components vue

---
+++ src/components/CalculatorMenu.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="menu mb-4">
+    <CalculatorButton 
+      v-for="button in buttons" 
+      :key="button" 
+      :label="button" 
+      @click="onButtonClick" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import CalculatorButton from './CalculatorButton.vue';
+
+export default defineComponent({
+  name: 'CalculatorMenu',
+  components: {
+    CalculatorButton
+  },
+  data() {
+    return {
+      buttons: [
+        '7', '8', '9', '/',
+        '4', '5', '6', '*',
+        '1', '2', '3', '-',
+        '0', 'C', '=', '+'
+      ]
+    };
+  },
+  methods: {
+    onButtonClick(button: string) {
+      this.$emit('button-click', button);
+    }
+  }
+});
+</script>
