faf367020862f478c369bb7609d9e2e864fccdab
Vitor Pires <88112014+vikpires@users.noreply.github.com>
2024-06-16 16:44:51 -0300

    add style features and fix some bugs (#9)

--- src/components/CalculatorButton.vue
+++ src/components/CalculatorButton.vue
@@ -1,6 +1,6 @@
 <template>
   <button 
-    class="btn p-4 m-1 rounded-xl bg-gray-700 text-gray-100 text-3xl hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-600 h-18 w-16 content-center" 
+    class="btn p-4 m-1 rounded-xl font-semibold text-2xl hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-700 h-18 w-16 active:scale-90 object-center" 
     @click="$emit('click', label)">
     {{ label }}
   </button>
--- src/components/CalculatorDisplay.vue
+++ src/components/CalculatorDisplay.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="display my-6 mx-1 p-4 bg-gray-300 text-right  text-gray-800 text-4xl rounded-xl h-20 whitespace-nowrap overflow-hidden content-center">
-    {{ value }}
-    <span class="cursor-blink"></span>
+  <div class="display my-8 mx-1 p-2 bg-gray-300 text-right text-gray-900 text-4xl rounded-xl h-28 object-center relative">
+    <span class="value p-2 text-ellipsis whitespace-break-spaces mr-1">{{ value }}</span>
+    <span v-if="shouldShowCursor" class="cursor-blink absolute right-4 m-auto"></span>
     </div>
 </template>
 
 <script lang="ts">
-import { defineComponent} from 'vue';
+import { defineComponent, computed} from 'vue';
 
 export default defineComponent({
   name: 'CalculatorDisplay',
@@ -15,6 +15,44 @@ export default defineComponent({
       type: String,
       required: true
     }
+  },
+  setup(props) {
+    const shouldShowCursor = computed(() => {
+    // Mostra o cursor piscante se o valor for diferente de '0' 
+    return props.value !== '0';
+    });
+    return {
+      shouldShowCursor
+    };
   }
 });
 </script>
+<style scoped>
+.display {
+  max-width:285px;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: nowrap; 
+  position: relative;
+  display: flex;
+  align-items: center; /* Centraliza verticalmente */
+  justify-content: right; /* Centraliza horizontalmente */
+}
+
+.cursor-blink {
+  display: inline-block;
+  width: 2px; /* Largura do cursor */
+  height: 64px; /* Altura do cursor */
+  background-color: rgb(31 41 55); /* Cor do cursor */
+  animation: blink 1s infinite; /* Animação de piscar */
+}
+
+@keyframes blink {
+  0%, 50% {
+    opacity: 0; /* Começa invisível */
+  }
+  51%, 100% {
+    opacity: 1; /* Fica visível */
+  }
+}
+</style>
