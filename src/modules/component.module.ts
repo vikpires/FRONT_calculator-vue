386d165d2092986f870b58b33017710743d8fe4c
Vitor Pires <88112014+vikpires@users.noreply.github.com>
2024-12-06 13:11:31 -0300

    [Feature] Add footer component

--- src/modules/component.module.ts
+++ src/modules/component.module.ts
@@ -3,11 +3,12 @@ import CalculatorButton from '../components/CalculatorButton.vue';
 import CalculatorDisplay from '../components/CalculatorDisplay.vue';
 import CalculatorMenu from '../components/CalculatorMenu.vue';
 import Calculator from '../view/Calculator.vue';
+import Footer from '../components/Footer.vue';
 
-
-export function setComponent(app:App) {
+export function setComponent(app: App) {
     app.component('CalculatorButton', CalculatorButton);
     app.component('CalculatorDisplay', CalculatorDisplay);
     app.component('CalculatorMenu', CalculatorMenu);
     app.component('Calculator', Calculator);
+    app.component('Footer', Footer);
 }
