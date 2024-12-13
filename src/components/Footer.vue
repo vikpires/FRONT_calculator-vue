e7d4b620774ca955c98d8bff7403b48b10d2905c
Vitor Pires <88112014+vikpires@users.noreply.github.com>
2024-12-13 08:19:56 -0300

    [Refactor] Add cite tag in footer component and update readme

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,11 +1,14 @@
 <template>
     <footer>
-        <p class="text-center text-gray-700 text-base">
-            Projeto desenvolvido por
-            <a href="https://github.com/thebiatriz" class="hover:text-blue-500 hover:underline" target="_blank"
-                rel="noopener noreferrer">Beatriz</a> e
-            <a href="https://github.com/vikpires" class="hover:text-blue-500 hover:underline" target="_blank"
-                rel="noopener noreferrer">Vitor</a>.
+        <p>
+            <cite class="text-center text-gray-700 text-base">
+                Projeto desenvolvido por
+                <a href="https://github.com/thebiatriz" class="hover:text-blue-500 underline" target="_blank"
+                    rel="noopener noreferrer">Beatriz</a> e
+
+                <a href="https://github.com/vikpires" class="hover:text-blue-500 underline" target="_blank"
+                    rel="noopener noreferrer">Vitor</a>.
+            </cite>
         </p>
     </footer>
 </template>
@@ -14,6 +17,6 @@
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-    name: 'footer'
+    name: 'Footer'
 });
 </script>
